7d6d7c3ffac2906b33136ef42de8e19959824d81
AbhinavShaw09 <shawabhinav93@gmail.com>
2023-09-26 07:38:59 +0530

    changed the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Footer from '../components/Footer'
-import Projects from '../components/Projects'
-import Skill from '../components/Skill'
-import ContactMe from '../components/ContactMe'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+import Projects from './components/Projects'
+import Skill from './components/Skill'
+import ContactMe from './components/ContactMe'
 import './App.css'
 
 function App() {
